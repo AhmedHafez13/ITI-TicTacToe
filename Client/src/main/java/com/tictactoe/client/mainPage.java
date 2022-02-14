a7902c8b6a5e959aa9c60855652466dc712ef5b6
YaraMohammed98 <yara.mohammed.samir@gmail.com>
2022-02-14 17:21:27 +0200

    last edit in mainpage

--- Client/src/main/java/com/tictactoe/client/mainPage.java
+++ Client/src/main/java/com/tictactoe/client/mainPage.java
@@ -1,60 +1,71 @@
-package models;
+package com.tictactoe.client;
 
+import com.gluonhq.charm.glisten.control.Avatar;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.effect.Blend;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.RadialGradient;
-import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
-public abstract class mainPage extends Pane {
+public abstract class mainPage extends ScrollPane {
 
     protected final Pane pane;
     protected final Rectangle rectangle;
     protected final Rectangle rectangle0;
-    protected final Rectangle rectangle1;
-    protected final ScrollBar scrollBar;
     protected final TextField textField;
-    protected final Rectangle rectangle2;
     protected final Button button;
     protected final ImageView imageView;
+    protected final ScrollPane scrollPane;
+    protected final AnchorPane anchorPane;
+    protected final Pane pane0;
+    protected final Avatar avatar;
     protected final TextField textField0;
-    protected final Rectangle rectangle3;
-    protected final TextField textField1;
-    protected final Blend blend;
-    protected final ImageView imageView0;
     protected final Button button0;
+    protected final Rectangle rectangle1;
     protected final Button button1;
+    protected final TextField textField1;
+    protected final Rectangle rectangle2;
+    protected final ImageView imageView0;
+    protected final Button button2;
     protected final ImageView imageView1;
+    protected final Button button3;
     protected final ImageView imageView2;
+    protected final ImageView imageView3;
+    protected final InnerShadow innerShadow;
+    protected final ImageView imageView4;
 
     public mainPage() {
 
         pane = new Pane();
         rectangle = new Rectangle();
         rectangle0 = new Rectangle();
-        rectangle1 = new Rectangle();
-        scrollBar = new ScrollBar();
         textField = new TextField();
-        rectangle2 = new Rectangle();
         button = new Button();
         imageView = new ImageView();
+        scrollPane = new ScrollPane();
+        anchorPane = new AnchorPane();
+        pane0 = new Pane();
+        avatar = new Avatar();
         textField0 = new TextField();
-        rectangle3 = new Rectangle();
-        textField1 = new TextField();
-        blend = new Blend();
-        imageView0 = new ImageView();
         button0 = new Button();
+        rectangle1 = new Rectangle();
         button1 = new Button();
+        textField1 = new TextField();
+        rectangle2 = new Rectangle();
+        imageView0 = new ImageView();
+        button2 = new Button();
         imageView1 = new ImageView();
+        button3 = new Button();
         imageView2 = new ImageView();
+        imageView3 = new ImageView();
+        innerShadow = new InnerShadow();
+        imageView4 = new ImageView();
 
         setMaxHeight(USE_PREF_SIZE);
         setMaxWidth(USE_PREF_SIZE);
@@ -62,6 +73,7 @@ public abstract class mainPage extends Pane {
         setMinWidth(USE_PREF_SIZE);
         setPrefHeight(500.0);
         setPrefWidth(700.0);
+        setStyle("-fx-background-color: #a18c7e;");
 
         pane.setLayoutX(445.0);
         pane.setLayoutY(121.0);
@@ -70,174 +82,232 @@ public abstract class mainPage extends Pane {
 
         rectangle.setArcHeight(5.0);
         rectangle.setArcWidth(5.0);
-        rectangle.setFill(javafx.scene.paint.Color.WHITE);
-        rectangle.setHeight(364.0);
-        rectangle.setLayoutX(-5.0);
+        rectangle.setFill(javafx.scene.paint.Color.valueOf("#f5ece600"));
+        rectangle.setHeight(366.0);
+        rectangle.setLayoutX(-8.0);
+        rectangle.setLayoutY(-2.0);
         rectangle.setStroke(javafx.scene.paint.Color.BLACK);
         rectangle.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
         rectangle.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
         rectangle.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        rectangle.setWidth(218.0);
+        rectangle.setWidth(224.0);
 
         rectangle0.setArcHeight(5.0);
         rectangle0.setArcWidth(5.0);
-        rectangle0.setFill(javafx.scene.paint.Color.valueOf("#bcf2a7"));
-        rectangle0.setHeight(50.0);
-        rectangle0.setLayoutX(6.0);
-        rectangle0.setLayoutY(14.0);
-        rectangle0.setStroke(javafx.scene.paint.Color.valueOf("#d3c1c1"));
+        rectangle0.setFill(javafx.scene.paint.Color.valueOf("#21402b"));
+        rectangle0.setHeight(61.0);
+        rectangle0.setLayoutX(4.0);
+        rectangle0.setLayoutY(5.0);
+        rectangle0.setStroke(javafx.scene.paint.Color.BLACK);
         rectangle0.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
         rectangle0.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
         rectangle0.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         rectangle0.setStrokeWidth(2.0);
-        rectangle0.setWidth(200.0);
+        rectangle0.setWidth(204.0);
+
+        textField.setAlignment(javafx.geometry.Pos.CENTER);
+        textField.setEditable(false);
+        textField.setLayoutX(14.0);
+        textField.setLayoutY(19.0);
+        textField.setPrefHeight(34.0);
+        textField.setPrefWidth(132.0);
+        textField.setStyle("-fx-background-color: none; -fx-text-fill: white; -fx-region-border: 0; -fx-font-size: 18;");
+        textField.setText("Online Players");
+        textField.setFont(new Font("Bodoni MT Bold", 18.0));
+        textField.setCursor(Cursor.NONE);
+
+        button.setLayoutX(144.0);
+        button.setLayoutY(24.0);
+        button.setMnemonicParsing(false);
+        button.setPrefHeight(25.0);
+        button.setPrefWidth(35.0);
+        button.setStyle("-fx-background-color: none;");
+
+        imageView.setFitHeight(26.0);
+        imageView.setFitWidth(35.0);
+        imageView.setPickOnBounds(true);
+        imageView.setPreserveRatio(true);
+        imageView.setImage(new Image(getClass().getResource("../../../images/refresh.png").toExternalForm()));
+        imageView.setCursor(Cursor.HAND);
+        button.setGraphic(imageView);
+        button.setCursor(Cursor.HAND);
+
+        scrollPane.setLayoutX(1.0);
+        scrollPane.setLayoutY(72.0);
+        scrollPane.setPrefHeight(288.0);
+        scrollPane.setPrefWidth(208.0);
+        scrollPane.setStyle("-fx-background: #21402b; -fx-border-color: black;");
+
+        anchorPane.setMinHeight(0.0);
+        anchorPane.setMinWidth(0.0);
+        anchorPane.setStyle("-fx-background-color: #21402b;");
+
+        pane0.setLayoutX(7.0);
+        pane0.setLayoutY(1.0);
+        pane0.setPrefHeight(61.0);
+        pane0.setPrefWidth(183.0);
+
+        avatar.setLayoutX(6.0);
+        avatar.setLayoutY(15.0);
+        avatar.setImage(new Image(getClass().getResource("../../../images/avatar1.png").toExternalForm()));
+
+        textField0.setLayoutX(52.0);
+        textField0.setLayoutY(20.0);
+        textField0.setPrefHeight(29.0);
+        textField0.setPrefWidth(71.0);
+        textField0.setStyle("-fx-background-color: none; -fx-text-fill: white; -fx-region-border: 0; -fx-font-size: 15;");
+        textField0.setText("Player 1");
+
+        button0.setCacheShape(false);
+        button0.setFocusTraversable(false);
+        button0.setGraphicTextGap(0.0);
+        button0.setLayoutX(123.0);
+        button0.setLayoutY(17.0);
+        button0.setMinWidth(53.0);
+        button0.setMnemonicParsing(false);
+        button0.setNodeOrientation(javafx.geometry.NodeOrientation.RIGHT_TO_LEFT);
+        button0.setPrefHeight(34.0);
+        button0.setPrefWidth(53.0);
+        button0.setStyle("-fx-background-color: none; -fx-font-size: 12; -fx-text-fill: #dedc66; -fx-border-width: 2; -fx-border-color: #dedc66; -fx-border-radius: 15;");
+        button0.setText("Invite");
+        button0.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+        button0.setTextOverrun(javafx.scene.control.OverrunStyle.CENTER_WORD_ELLIPSIS);
+        button0.setCursor(Cursor.HAND);
+        scrollPane.setContent(anchorPane);
 
         rectangle1.setArcHeight(5.0);
         rectangle1.setArcWidth(5.0);
-        rectangle1.setFill(javafx.scene.paint.Color.valueOf("#bcf2a7"));
-        rectangle1.setHeight(287.0);
-        rectangle1.setLayoutX(6.0);
-        rectangle1.setLayoutY(72.0);
-        rectangle1.setStroke(javafx.scene.paint.Color.valueOf("#d3c1c1"));
+        rectangle1.setFill(javafx.scene.paint.Color.valueOf("#21402b"));
+        rectangle1.setHeight(86.0);
+        rectangle1.setLayoutX(22.0);
+        rectangle1.setLayoutY(25.0);
+        rectangle1.setSmooth(false);
+        rectangle1.setStroke(javafx.scene.paint.Color.BLACK);
+        rectangle1.setStrokeDashOffset(10.0);
         rectangle1.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
         rectangle1.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
+        rectangle1.setStrokeMiterLimit(50.0);
         rectangle1.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         rectangle1.setStrokeWidth(2.0);
-        rectangle1.setWidth(200.0);
+        rectangle1.setWidth(641.0);
 
-        scrollBar.setLayoutX(188.0);
-        scrollBar.setLayoutY(72.0);
-        scrollBar.setOrientation(javafx.geometry.Orientation.VERTICAL);
-        scrollBar.setPrefHeight(287.0);
-        scrollBar.setPrefWidth(18.0);
-        
-        textField.setAlignment(javafx.geometry.Pos.CENTER);
-        textField.setLayoutX(49.0);
-        textField.setLayoutY(27.0);
-        textField.setPrefHeight(25.0);
-        textField.setPrefWidth(115.0);
-        textField.setText("Online Players");
+        button1.setLayoutX(58.0);
+        button1.setLayoutY(62.0);
+        button1.setMnemonicParsing(false);
+        button1.setPrefHeight(25.0);
+        button1.setPrefWidth(36.0);
+        button1.setStyle("-fx-background-color: none;");
+        button1.setTextFill(javafx.scene.paint.Color.valueOf("#895f99"));
+        button1.setCursor(Cursor.HAND);
+
+        textField1.setAlignment(javafx.geometry.Pos.CENTER);
+        textField1.setLayoutX(154.0);
+        textField1.setLayoutY(38.0);
+        textField1.setPrefHeight(61.0);
+        textField1.setPrefWidth(351.0);
+        textField1.setStyle("-fx-background-color: none; -fx-text-fill: white; -fx-font-size: 30; -fx-region-border: 0;");
+        textField1.setText("Player Name");
+        textField1.setFont(new Font("Bodoni MT Bold", 30.0));
 
         rectangle2.setArcHeight(5.0);
         rectangle2.setArcWidth(5.0);
-        rectangle2.setFill(javafx.scene.paint.Color.valueOf("#dddddd"));
-        rectangle2.setHeight(71.0);
+        rectangle2.setFill(javafx.scene.paint.Color.valueOf("#21402b"));
+        rectangle2.setHeight(368.0);
         rectangle2.setLayoutX(26.0);
-        rectangle2.setLayoutY(36.0);
-        rectangle2.setSmooth(false);
-        rectangle2.setStroke(javafx.scene.paint.Color.valueOf("#bcbcbc"));
-        rectangle2.setStrokeDashOffset(10.0);
+        rectangle2.setLayoutY(118.0);
+        rectangle2.setStroke(javafx.scene.paint.Color.BLACK);
         rectangle2.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
         rectangle2.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
-        rectangle2.setStrokeMiterLimit(50.0);
         rectangle2.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        rectangle2.setStrokeWidth(2.0);
-        rectangle2.setWidth(631.0);
-
-        button.setLayoutX(56.0);
-        button.setLayoutY(56.0);
-        button.setMnemonicParsing(false);
-        button.setPrefHeight(25.0);
-        button.setPrefWidth(36.0);
-
-        imageView.setFitHeight(24.0);
-        imageView.setFitWidth(113.0);
-        imageView.setPickOnBounds(true);
-        imageView.setPreserveRatio(true);
-        imageView.setImage(new Image(getClass().getResource("../../../../images/back.png").toExternalForm()));
-        button.setGraphic(imageView);
-
-        textField0.setAlignment(javafx.geometry.Pos.CENTER);
-        textField0.setLayoutX(216.0);
-        textField0.setLayoutY(52.0);
-        textField0.setPrefHeight(40.0);
-        textField0.setPrefWidth(251.0);
-       
-        textField0.setText("Player Name");
-        textField0.setFont(new Font("Bodoni MT", 16.0));
-
-        rectangle3.setArcHeight(5.0);
-        rectangle3.setArcWidth(5.0);
-        rectangle3.setFill(javafx.scene.paint.Color.WHITE);
-        rectangle3.setHeight(364.0);
-        rectangle3.setLayoutX(27.0);
-        rectangle3.setLayoutY(121.0);
-        rectangle3.setStroke(javafx.scene.paint.Color.BLACK);
-        rectangle3.setStrokeLineCap(javafx.scene.shape.StrokeLineCap.ROUND);
-        rectangle3.setStrokeLineJoin(javafx.scene.shape.StrokeLineJoin.ROUND);
-        rectangle3.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        rectangle3.setWidth(396.0);
-
-        textField1.setAlignment(javafx.geometry.Pos.CENTER);
-        textField1.setEditable(false);
-        textField1.setLayoutX(111.0);
-        textField1.setLayoutY(138.0);
-        textField1.setPrefHeight(32.0);
-        textField1.setPrefWidth(211.0);
-        textField1.setText("Tic-Tac-Toe");
-        textField1.setFont(new Font("Book Antiqua", 25.0));
-
-        textField1.setEffect(blend);
+        rectangle2.setWidth(398.0);
 
         imageView0.setFitHeight(132.0);
         imageView0.setFitWidth(132.0);
-        imageView0.setLayoutX(159.0);
-        imageView0.setLayoutY(195.0);
+        imageView0.setLayoutX(168.0);
+        imageView0.setLayoutY(202.0);
         imageView0.setPickOnBounds(true);
         imageView0.setPreserveRatio(true);
-        imageView0.setImage(new Image(getClass().getResource("../../../../images/tic-tac-toe%20(1).png").toExternalForm()));
+        imageView0.setImage(new Image(getClass().getResource("../../../images/tic-tac-toe%20(1).png").toExternalForm()));
 
-        button0.setDefaultButton(true);
-        button0.setLayoutX(139.0);
-        button0.setLayoutY(335.0);
-        button0.setMnemonicParsing(false);
-        button0.setPrefHeight(40.0);
-        button0.setPrefWidth(172.0);
-        button0.setText("        Play With PC");
-        button0.setTextFill(javafx.scene.paint.Color.WHITE);
-        button0.setFont(new Font("Aldhabi", 23.0));
-
-        button1.setDefaultButton(true);
-        button1.setLayoutX(139.0);
-        button1.setLayoutY(390.0);
-        button1.setMnemonicParsing(false);
-        button1.setPrefHeight(40.0);
-        button1.setPrefWidth(172.0);
-        button1.setText("     Leader Board");
-        button1.setTextFill(javafx.scene.paint.Color.WHITE);
+        button2.setLayoutX(139.0);
+        button2.setLayoutY(343.0);
+        button2.setMnemonicParsing(false);
+        button2.setPrefHeight(50.0);
+        button2.setPrefWidth(172.0);
+        button2.setStyle("-fx-background-color: #8c522b; -fx-font-size: 15;");
+        button2.setText(" Play With PC");
+        button2.setTextFill(javafx.scene.paint.Color.WHITE);
+        button2.setFont(new Font("Aldhabi", 27.0));
+        button2.setCursor(Cursor.HAND);
 
         imageView1.setFitHeight(27.0);
         imageView1.setFitWidth(38.0);
-        imageView1.setLayoutX(152.0);
-        imageView1.setLayoutY(342.0);
         imageView1.setPickOnBounds(true);
         imageView1.setPreserveRatio(true);
-        imageView1.setImage(new Image(getClass().getResource("../../../../images/playstation.png").toExternalForm()));
+        imageView1.setImage(new Image(getClass().getResource("../../../images/playstation.png").toExternalForm()));
+        button2.setGraphic(imageView1);
 
-        imageView2.setFitHeight(40.0);
+        button3.setDefaultButton(true);
+        button3.setLayoutX(139.0);
+        button3.setLayoutY(400.0);
+        button3.setMnemonicParsing(false);
+        button3.setPrefHeight(50.0);
+        button3.setPrefWidth(172.0);
+        button3.setStyle("-fx-background-color: #8c522b; -fx-font-size: 15;");
+        button3.setText("Leader Board");
+        button3.setTextFill(javafx.scene.paint.Color.WHITE);
+        button3.setFont(new Font("Aldhabi", 27.0));
+        button3.setCursor(Cursor.HAND);
+
+        imageView2.setFitHeight(35.0);
         imageView2.setFitWidth(40.0);
-        imageView2.setLayoutX(146.0);
-        imageView2.setLayoutY(390.0);
         imageView2.setPickOnBounds(true);
         imageView2.setPreserveRatio(true);
-        imageView2.setImage(new Image(getClass().getResource("../../../../images/crown%20(1).png").toExternalForm()));
+        imageView2.setImage(new Image(getClass().getResource("../../../images/crown%20(1).png").toExternalForm()));
+        button3.setGraphic(imageView2);
+
+        imageView3.setFitHeight(61.0);
+        imageView3.setFitWidth(258.0);
+        imageView3.setLayoutX(96.0);
+        imageView3.setLayoutY(133.0);
+        imageView3.setPickOnBounds(true);
+        imageView3.setImage(new Image(getClass().getResource("../../../images/9.png").toExternalForm()));
+
+        innerShadow.setChoke(0.76);
+        innerShadow.setColor(javafx.scene.paint.Color.#070000);
+        innerShadow.setHeight(10.5);
+        innerShadow.setRadius(2.125);
+        innerShadow.setWidth(0.0);
+        imageView3.setEffect(innerShadow);
+
+        imageView4.setFitHeight(38.0);
+        imageView4.setFitWidth(76.0);
+        imageView4.setLayoutX(57.0);
+        imageView4.setLayoutY(53.0);
+        imageView4.setPickOnBounds(true);
+        imageView4.setPreserveRatio(true);
+        imageView4.setImage(new Image(getClass().getResource("../../../images/arrow.png").toExternalForm()));
+        imageView4.setCursor(Cursor.HAND);
 
         pane.getChildren().add(rectangle);
         pane.getChildren().add(rectangle0);
-        pane.getChildren().add(rectangle1);
-        pane.getChildren().add(scrollBar);
         pane.getChildren().add(textField);
+        pane.getChildren().add(button);
+        pane0.getChildren().add(avatar);
+        pane0.getChildren().add(textField0);
+        pane0.getChildren().add(button0);
+        anchorPane.getChildren().add(pane0);
+        pane.getChildren().add(scrollPane);
         getChildren().add(pane);
-        getChildren().add(rectangle2);
-        getChildren().add(button);
-        getChildren().add(textField0);
-        getChildren().add(rectangle3);
-        getChildren().add(textField1);
-        getChildren().add(imageView0);
-        getChildren().add(button0);
+        getChildren().add(rectangle1);
         getChildren().add(button1);
-        getChildren().add(imageView1);
-        getChildren().add(imageView2);
+        getChildren().add(textField1);
+        getChildren().add(rectangle2);
+        getChildren().add(imageView0);
+        getChildren().add(button2);
+        getChildren().add(button3);
+        getChildren().add(imageView3);
+        getChildren().add(imageView4);
 
     }
 }
